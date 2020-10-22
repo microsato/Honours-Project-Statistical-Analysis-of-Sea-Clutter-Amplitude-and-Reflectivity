1f792889ff7ca4a674bed1e4c30ee764ef72d0ad
Mic Rosato <RSTMIC005@myuct.ac.za>
2020-10-22 02:41:47 +0200

    Added all files, changed GIT, TSC, HYB

--- Fynmeet.m
+++ Fynmeet.m
@@ -1,10 +1,22 @@
 clear all;
 close all;
 %% Global Variables
+for i=1:1
 global path
 global RangeBin
+global R
+global f
+global A
+global lambda
+global SS
+global pol
 global DataSet
 global GrazingAngle
+global elevation 
+global azimuth
+global theta_w
+global swh
+global wndspd
 global data 
 global x 
 global p_mle_rayl
@@ -25,31 +37,214 @@ global modChiSqr_wbl_mom
 global modChiSqr_k_watts
 global modChiSqr_k_mom
 global modChiSqr_k_r
+end
 %% Load Dataset
-%load('TFC15_008');         % loads a variables Cdata, Info, NumOfPRIs, NumOfRangeBins, PCI, PRI_s, Waveform, lamda  
-% DataSetName = 'CFA17_001';
-%DataSetName = 'CFA17_002';
-DataSetName = 'CFA17_001';
+% loads a variables Cdata, Info, NumOfPRIs, NumOfRangeBins, PCI, PRI_s, Waveform, lamda  
+% Choose between CFA17_001, CFA17_002, CFA17_003, CFC17_001 , CFC14_004 ,
+% CFC16_001
+DataSetName = '11_011_CS';
 load(DataSetName);
 DataSet = DataSetName(7:9);
 %% Range Bin
-RangeBin = 2;               % Specify range bin to process 
+RangeBin = 1;               % Specify range bin to process 
 %% Path for Figures
-path='C:\Users\micro\.QtWebEngineProcess\Desktop\EEE4022S\DrHerselman\';
+path='C:\Users\micro\.QtWebEngineProcess\Desktop\EEE4022S\Figures';
 %% Radar parameters
-c = 299792458;
-lambda=c/(9*(10^9));                         % Wavelength of the radar
+c = 299792458;                      % Wavelength of the radar
 Bandwidth_Hz = 10e6;                         % Pulsewidth = 100us, pure sinusoid at 9GHz
 RangeResolution_m = c/(2*Bandwidth_Hz);      % Range resolution in meters 
-if DataSet == '001'
-    GrazingAngle = (1.02+1.27)/2;
-elseif DataSet =='002'
-     GrazingAngle = (0.472+0.525)/2;
-else 
-     GrazingAngle =(0.323+0.35)/2;
-end 
-
-
+for setParameters = 1:1
+    if DataSetName == 'CFA17_001'
+        pol = 'VV';
+        f = 6.9;
+        GrazingAngle = (1.02+1.27)/2; 
+        wndspd = 14.5; %kts
+        wind_gust = 23.7; %kts
+        avg_wind = 13.8; %kts
+        swh = 2.31; %m
+        azimuth = 2;
+        ant_az = 180.7;
+        elevation = -1.225; %degrees
+        R = PCI.Range;
+        theta_w = 253.4 -ant_az;
+        lambda = c/(f*10^9);
+    elseif DataSetName =='CFA17_002'
+        pol = 'VV';
+        f = 6.9;
+        GrazingAngle = (0.472+0.525)/2;
+        wndspd = 14.4; %kts
+        wind_gust = 23.7; %kts
+        avg_wind = 13.8; %kts
+        swh = 2.31; %m
+        azimuth = 2;
+        ant_az = 180.7;
+        elevation = -1.225; %degrees
+        R = PCI.Range;
+        theta_w = 253.4 -ant_az;
+         lambda = c/(f*10^9);
+    elseif DataSetName == 'CFA17_003'
+        pol = 'VV';
+        f = 6.9;
+        GrazingAngle =(0.323+0.35)/2;
+        inst_wind = 14.4; %kts
+        wind_gust = 23.7; %kts
+        avg_wind = 13.8; %kts
+        swh = 2.3; %m
+        azimuth = 2;
+        ant_az = 180.7;
+        elevation = -1.219; %degrees
+        R = PCI.Range;
+         lambda = c/(f*10^9);
+    elseif DataSetName == 'CFC17_001'
+        pol = 'VV';
+        f = 9;
+        GrazingAngle =(0.853+1.27)/2;
+        wndspd = 15.5; %kts
+        wind_gust = 29.2; %kts
+        avg_wind = 11.4; %kts
+        swh = 2.22; %m
+        azimuth = 1.8;
+        ant_az = 165.5;
+        elevation = -1.06; %degrees
+        R = PCI.Range;  
+         lambda = c/(f*10^9);
+    elseif DataSetName == '08_017_CS'
+        pol = 'VV';
+        f = 8.8;  
+        GrazingAngle =(0.293 + 0.314 )/2;
+        wndspd = 5.64; %kts
+        wind_gust = 29.2; %kts
+        avg_wind = 3.84; %kts
+        swh = 2.44; %m
+        azimuth = 1.8;
+        ant_az = 240;
+        elevation = -0.571; %degrees
+        R = PCI.Range;   
+        A = 15*R*(azimuth*pi/180)*sec(GrazingAngle*pi/180);
+        lambda = c/(f*10^9);
+     elseif DataSetName == '08_018_CS'
+        pol = 'VV';
+        f = 8.8;  
+        GrazingAngle =(0.293 + 0.314 )/2;
+        wndspd = 5.64; %kts
+        wind_gust = 29.2; %kts
+        avg_wind = 3.84; %kts
+        swh = 2.44; %m
+        azimuth = 1.8;
+        ant_az = 262.5;
+        elevation = -0.571; %degrees
+        R = PCI.Range;   
+        A = 15*R*(azimuth*pi/180)*sec(GrazingAngle*pi/180);
+        lambda = c/(f*10^9);
+      elseif DataSetName == '08_019_CS'
+        pol = 'VV';
+        f = 8.8;  
+        GrazingAngle =(0.293 + 0.314 )/2;
+        wndspd = 6.61; %kts
+        wind_gust = 29.2; %kts
+        avg_wind = 4.62; %kts
+        swh = 2.21; %m
+        azimuth = 1.8;
+        ant_az = 285;
+        elevation = -0.571; %degrees
+        R = PCI.Range;   
+        A = 15*R*(azimuth*pi/180)*sec(GrazingAngle*pi/180);
+        lambda = c/(f*10^9);
+    elseif DataSetName == '08_020_CS'
+        pol = 'VV';
+        f = 8.8;  
+        GrazingAngle =(0.293 + 0.314 )/2;
+        wndspd = 6.61; %kts
+        wind_gust = 29.2; %kts
+        avg_wind = 4.62; %kts
+        swh = 2.21; %m
+        azimuth = 1.8;
+        ant_az = 307.5;
+        elevation = -0.569; %degrees
+        R = PCI.RangeS;   
+        A = 15*R*(azimuth*pi/180)*sec(GrazingAngle*pi/180);
+        lambda = c/(f*10^9);
+     elseif DataSetName == '08_021_CS'
+        pol = 'VV';
+        f = 8.8;  
+        GrazingAngle =(0.293 + 0.314 )/2;
+        wndspd = 6.61; %kts
+        wind_gust = 29.2; %kts
+        avg_wind = 4.62; %kts
+        swh = 2.21; %m
+        azimuth = 1.8;
+        ant_az = 330;
+        elevation = -0.5711; %degrees
+        R = PCI.Range;   
+        A = 15*R*(azimuth*pi/180)*sec(GrazingAngle*pi/180);
+        lambda = c/(f*10^9);
+    elseif DataSetName == '11_007_CS'
+        pol = 'VV';
+        f = 8.8;  
+        GrazingAngle =(0.0958 + 0.108)/2;
+        wndspd = 15.6; %kts
+        avg_wind = 8.29; %kts
+        swh = 2.78; %m
+        azimuth = 1.8;
+        ant_az = 240;
+        elevation = -0.4944; %degrees
+        R = PCI.Range;   
+        A = 15*R*(azimuth*pi/180)*sec(GrazingAngle*pi/180);
+        lambda = c/(f*10^9);
+    elseif DataSetName == '11_008_CS'
+        pol = 'VV';
+        f = 8.8;  
+        GrazingAngle =(0.0958 + 0.108)/2;
+        wndspd = 15.6; %kts
+        avg_wind = 8.29; %kts
+        swh = 2.78; %m
+        azimuth = 1.8;
+        ant_az = 262.5;
+        elevation = -0.4944; %degrees
+        R = PCI.Range;   
+        A = 15*R*(azimuth*pi/180)*sec(GrazingAngle*pi/180);
+        lambda = c/(f*10^9);
+     elseif DataSetName == '11_009_CS'
+        pol = 'VV';
+        f = 8.8;  
+        GrazingAngle =(0.0958 + 0.108)/2;
+        wndspd = 15.6; %kts
+        avg_wind = 8.29; %kts
+        swh = 2.78; %m
+        azimuth = 1.8;
+        ant_az = 285;
+        elevation = -0.4944; %degrees
+        R = PCI.Range;   
+        A = 15*R*(azimuth*pi/180)*sec(GrazingAngle*pi/180);
+        lambda = c/(f*10^9);    
+     elseif DataSetName == '11_010_CS'
+        pol = 'VV';
+        f = 8.8;  
+        GrazingAngle =(0.0958 + 0.108)/2;
+        wndspd = 15.6; %kts
+        avg_wind = 8.29; %kts
+        swh = 2.48; %m
+        azimuth = 1.8;
+        ant_az = 307.5;
+        elevation = -0.4944; %degrees
+        R = PCI.Range;   
+        A = 15*R*(azimuth*pi/180)*sec(GrazingAngle*pi/180);
+        lambda = c/(f*10^9);    
+    elseif DataSetName == '11_011_CS'
+        pol = 'VV';
+        f = 8.8;  
+        GrazingAngle =(0.0958 + 0.108)/2;
+        wndspd = 15.6; %kts
+        avg_wind = 8.29; %kts
+        swh = 2.48; %m
+        azimuth = 1.8;
+        ant_az = 330;
+        elevation = -0.4944; %degrees
+        R = PCI.Range;   
+        A = 15*R*(azimuth*pi/180)*sec(GrazingAngle*pi/180);
+        lambda = c/(f*10^9);    
+    end 
+end
 %% Plot Cdata 
 PRI = PRI_s; % TFC15_008
 VRange_m = (1:1:size(Cdata,2)); 
@@ -62,14 +257,17 @@ ts = 1/fs;
 NumRangeLines = size(Cdata,1);
 StartRangeLine = round(NumRangeLines/2); 
 StopRangeLine = round(NumRangeLines/4*3);
-% X = Cdata(StartRangeLine:StopRangeLine,RangeBin);
+X = Cdata(StartRangeLine:StopRangeLine,RangeBin);
 
 % DataOneBin = Cdata(:,RangeBin); %Extract the data from the specified bin only 
 % DataMeanSubtracted = DataOneBin - mean(DataOneBin);  %Normalise about mean ***CHECK*** 
 % data = abs(DataMeanSubtracted)'; % amplitude of the complex clutter
-%% Main
+%% Reflectivity 
 
-for RangeBin = 1:1:size(Cdata,2)
+reflectivityCompare(Cdata);
+%% Main - Amplitude Stats
+
+%for RangeBin = 1:1:size(Cdata,2)
 
 X = Cdata(StartRangeLine:StopRangeLine,RangeBin);
 
@@ -191,7 +389,17 @@ arr_modChiSqr_wbl_mom(i)=modChiSqr_wbl_mom;
 arr_modChiSqr_k_watts(i)=modChiSqr_k_watts;
 arr_modChiSqr_k_mom(i)=modChiSqr_k_mom;
 arr_modChiSqr_k_r(i)=modChiSqr_k_r;
-end 
+
+%% Call to Plot 
+% plotAll();      %plots all distributions 
+% plotRay();      %plots Rayleigh MLE and MoM
+% plotLogn();     %plots Lognormal MLE and MoM
+% plotWbl();      %plots Weibull MLE and MoM
+% plotK();        %plots K-Dsitribution MLE and MoM
+% plotBest();       %plots the best fit of each dsitribution
+% %close all;
+% figure()
+%end
 %% Average Test over Dataset
 av_modChiSqr_rayl_mle = mean(arr_modChiSqr_rayl_mle);
 av_modChiSqr_rayl_mom=mean(arr_modChiSqr_rayl_mom);
@@ -270,7 +478,7 @@ legend('Data','Rayleigh MLE','Rayleigh MoM','Lognormal MLE','Lognormal MoM','Wei
 t1 = sprintf('Range Bin: %i  Grazing Angle: %f.3',RangeBin, GrazingAngle);
 title(t1);
 baseFileName = sprintf('%s_%i_All',DataSet,RangeBin);
-saveas(figure(1),fullfile(path,[baseFileName '.jpeg']));
+saveas(figure(1),fullfile(path,[baseFileName '.png']));
 end 
 function plotRay()
 global path
@@ -303,7 +511,7 @@ legend('Data','Rayleigh MLE','Rayleigh MoM','Location','southwest');
 t1 = sprintf('Range Bin: %i  Grazing Angle: %.3f',RangeBin, GrazingAngle);
 title(t1);
 baseFileName = sprintf('%s_%i_Rayl',DataSet,RangeBin);
-saveas(figure(2),fullfile(path,[baseFileName '.jpeg']));
+saveas(figure(2),fullfile(path,[baseFileName '.png']));
 end 
 function plotLogn()
 % Plot histogram
@@ -337,7 +545,7 @@ legend('Data','Lognormal MLE','Lognormal MoM','Location','southwest');
 t1 = sprintf('Range Bin: %i  Grazing Angle: %.3f',RangeBin, GrazingAngle);
 title(t1);
 baseFileName = sprintf('%s_%i_LogN',DataSet,RangeBin);
-saveas(figure(3),fullfile(path,[baseFileName '.jpeg']));
+saveas(figure(3),fullfile(path,[baseFileName '.png']));
 end 
 function plotWbl()
 % Plot histogram
@@ -371,7 +579,7 @@ legend('Data','Weibull MLE','Weibull MoM','Location','southwest');
 t1 = sprintf('Range Bin: %i  Grazing Angle: %.3f',RangeBin, GrazingAngle);
 title(t1);
 baseFileName = sprintf('%s_%i_Wbl',DataSet,RangeBin);
-saveas(figure(4),fullfile(path,[baseFileName '.jpeg']));
+saveas(figure(4),fullfile(path,[baseFileName '.png']));
 end 
 function plotK()
 % Plot histogram
@@ -408,7 +616,7 @@ legend('Data','K-Distribution Watts','K-Distribution MoM','K-Distribution Raghav
 t1 = sprintf('Range Bin: %i  Grazing Angle: %.3f',RangeBin, GrazingAngle);
 title(t1);
 baseFileName = sprintf('%s_%i_K',DataSet,RangeBin);
-saveas(figure(5),fullfile(path,[baseFileName '.jpeg']));
+saveas(figure(5),fullfile(path,[baseFileName '.png']));
 end 
 function plotBest()
 % Plot histogram
@@ -495,7 +703,7 @@ legend('Data',l1,l2,l3,l4,'Location','southwest');
 t1 = sprintf('Range Bin: %i  Grazing Angle: %.3f',RangeBin, GrazingAngle);
 title(t1);
 baseFileName = sprintf('%s_%i_Best',DataSet,RangeBin);
-saveas(figure(6),fullfile(path,[baseFileName '.jpeg']));
+saveas(figure(6),fullfile(path,[baseFileName '.png']));
 end 
 %% Amplitude Distribution Fitting 
 function [p_mle,p_mom,sigma_mle,sigma_mom] = rayleighPDF(data)
@@ -712,8 +920,194 @@ function [modChiSqr_watts, modChiSqr_mom, modChiSqr_r]=modChiSqr_k(K,PFA,data,sh
     modChiSqr_r= sum((fi_r-(PFA)*N/K).^2/((PFA)*N/K));
     
 end 
-%% Reflectivity Empirical Models 
-% Add GIT Model, Nathonson's Table, HYB Model 
+%% Reflectivity 
+function [mean_reflectivity] = getReflectivity(Cdata)
+    global R
+    global azimuth
+    global GrazingAngle 
+    global elevation
+    global A
+    fprintf('Measured Reflectivity \n');
+    for RangeBin = 1:1:48
+        DataOneBin = Cdata(:,RangeBin); %Extract the data from the specified bin only 
+        DataMeanSubtracted = DataOneBin - mean(DataOneBin);  %Normalise about mean ***CHECK*** 
+        data = abs(DataMeanSubtracted); % amplitude of the complex clutter
+%         A = pi*R^2*tan((-elevation*(pi/180))/2)*tan((azimuth*(pi/180))/2)*csc(GrazingAngle*(pi/180));
+       
+        %A = 15*((pi/180)*azimuth*R);
+        sigma = mean((abs(data)).^2);
+        sigma_o_measured(RangeBin) = 10*log10(sigma/A);
+        fprintf('Range bin: %i  Reflectivity: %f \n',RangeBin, sigma_o_measured(RangeBin));
+        %R = R + 15;
+    end
+    fprintf('Average Reflectivity in Dataset: %f \n', round(mean(sigma_o_measured)));
+    mean_reflectivity = round(mean(sigma_o_measured));
+end
+%GIT Model
+function [mean_GIT] = GIT()
+%Get the normalised clutter reflectivity from the GIT reflectivity model 
+% gr : Grazing Angle in degrees to be converted to radians
+% wndvel : wind velocity kts to be converted to kts 
+% pol : radar polarization
+% theta_w : look direction relative to wnd direction in degrees to be converted
+% to radians
+%swh : significant wave height in m 
+% lambda : radar frequency in GHz
+    global pol 
+    global lambda
+    global GrazingAngle
+    global theta_w
+    global swh 
+    global SS
+%     global pol 
+    gr = GrazingAngle;
+    
+    %convert from degrees to radians
+    gr = deg2rad(gr);
+    theta_w = deg2rad(gr);
+
+    %get average wave heigh hav from swh
+    hav = 1.6*swh;
+    U = 3.189*((SS)^(0.8));
+    %Get adjustment factors Ga, Fu, Gw 
+    a  = (14.4*lambda + 5.5)*((gr*hav)/(lambda));
+    q = 1.1/((lambda + 0.015)^(0.4));
+
+    Ga = (a^4)/(1+a^4);
+    Gu = exp(0.2*cos(theta_w)*(1-2.8*gr)*(lambda + 0.015)^(-0.4));
+    Gw = ((1.94*U)/(1+U/15.4))^q;
+
+    HH = 10*log10(3.9*10^(-6)*lambda*(gr^0.4)*Ga*Gu*Gw);
+    if pol == 'HH' 
+        mean_GIT = HH;
+    elseif pol == 'VV'
+        mean_GIT = HH -1.05*log(hav+0.015) + 1.09*log(lambda) + 1.27*log(gr+0.0001) + 9.7;
+    end 
+end
+% Hybrid Model 
+function [mean_HYB] = HYB()
+    global theta_w
+    global f 
+    global swh
+    global GrazingAngle 
+    global pol 
+    global SS
+    global lambda
+    gr = GrazingAngle;
+
+    %get reference reflectivity for SS 5, gr = 0.1, Pol = VV, theta_w = 0;
+    sigma_ref = 24.4*log10(f) -65.2; % f in Ghz
+    %get RMS wave height sigma_h
+    sigma_h = 0.031*(SS^2); %this is the most common approximation used
+    sigma_h = swh/2.83;
+    %define reference angle in degrees
+    phi_ref = 0.1;
+    %define transitional angle in degrees
+    phi_t = asin(0.0632*lambda/sigma_h)*(180/pi);
+
+    if phi_t >= phi_ref
+        if gr < phi_ref
+            Kg = 0;
+        elseif (phi_ref<=gr)&& (gr<=phi_t)
+            Kg = 20*log10(gr/phi_ref);
+        elseif (phi_t <= gr) && (gr<=30)
+            Kg = 20*log10(phi_t/phi_ref) + 10*log10(gr/phi_t);
+        end
+    elseif phi_t < phi_ref
+        if gr <= phi_ref
+            Kg = 0;
+        elseif gr>phi_ref
+            Kg = 10*log10(gr/phi_ref);
+        end
+    end 
+
+    Ks = 5*(SS-5);
+    hav = 0.08*SS^2;
+    if pol == 'HH'
+        Kp = 1.1*log(hav + 0.015) - 1.1*log(lambda) -1.3*log(gr/57.3 + 0.0001) - 9.7;
+    elseif pol == 'VV'
+        Kp = 0;
+    end
+
+    Kd = (2+1.7*log10(0.1/lambda))*(cos(theta_w) - 1); 
+    mean_HYB = sigma_ref + Kg+ Ks + Kp + Kd;
+end
+%TSC Model 
+function [mean_TSC] = TSC()
+    global lambda
+    global theta_w 
+    global GrazingAngle
+    global f
+    global pol 
+    global SS
+    gr = deg2rad(GrazingAngle);
+    theta = deg2rad(theta_w);
+    %get surface height standard deviation in m 
+    sigma_z = 0.03505*SS^(1.95);
+    %get sigma_a 
+    sigma_a = 4.5416*gr*(3.2808*sigma_z+0.25)/lambda;
+    Ga = sigma_a^1.5/(1+sigma_a^1.5);
+    %get windspeed in m/s
+    U = 3.189*(SS^0.8);
+    %get constant terms
+    Q = gr^0.6;
+
+    A1 = (1+(lambda/0.00914)^3)^0.1;
+    A2 = (1+(lambda/0.00305)^3)^0.1;
+    A3 = (1+(lambda/0.00914)^3)^(Q/3);
+    A4 = 1+0.35*Q;
+    A = 2.63*A1/(A2*A3*A4);
+
+    Gw =((1.9438*U+4)/15)^(A);
+    Gu = exp(0.3*cos(theta)*exp(-gr/0.17)/(10.7636*lambda^2 + 0.005)^0.2);
+
+    HH = 10*log10(1.7*10^(-5)*(gr^(0.5))*Gu*Gw*Ga/((3.2802*lambda + 0.05)^1.8));
+    if pol == 'HH' 
+        mean_TSC = 10*log10(1.7*10^(-5)*gr^(0.5)*Gu*Gw*Ga/(3.2802*lambda + 0.05)^1.8);
+    elseif pol == 'VV' 
+        mean_TSC = HH  -1.05*log(8.225*sigma_z+0.05) +1.09*log(lambda) + 1.27*log(sin(gr) + 0.0001)+10.945;
+    end 
+end
+function [SS]  = Douglas()
+% Get the Douglas sea state given the significant wave height h1/3
+global swh
+global SS
+if (swh<0.333)
+    SS = 1;
+elseif (swh>=0.333) && (swh<0.9144)
+    SS = 2;
+elseif (swh>=0.9144) && (swh<1.5239)
+    SS = 3;
+elseif (swh>=1.5239) && (swh<2.4385)
+    SS = 4;
+elseif (swh>=2.4385) && (swh<3.657)
+    SS = 5;
+elseif (swh>=3.657) && (swh<6.0957)
+    SS = 6;
+elseif (swh>=6.0957) && (swh<12.1914)
+    SS = 7;
+end
+end
+function [] = reflectivityCompare(Cdata)
+    global SS 
+    global GrazingAngle
+    gr = GrazingAngle;
+    mean_reflectivity = getReflectivity(Cdata);
+    SS  = Douglas();
+    mean_GIT = GIT();
+    mean_HYB = HYB();
+    mean_TSC = TSC();
+    fprintf('Reflecitvity over the dataset:\n Mean Measured Reflectivity: %f \nGIT Model: %f\nHYB Model: %f\nTSC Model: %f\n',mean_reflectivity,mean_GIT,mean_HYB,mean_TSC);
+     [m,index] = min(abs([mean_GIT, mean_HYB, mean_TSC] - mean_reflectivity));
+     if index == 1
+         best = 'GIT';
+     elseif index == 2
+         best = 'HYB';
+     elseif index ==3
+         best = 'TSC';
+     end
+    fprintf('Best Model: %s\n',best);
+end
 %% Miscellaneous Functions
 function y=rag(x)
                 nu=[0.05 0.1:0.05:5];
